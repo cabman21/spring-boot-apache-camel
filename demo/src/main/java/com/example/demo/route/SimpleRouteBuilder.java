27466d067e7788fa358513c742ba9903bf26d73e
Jinhae Kim <cabman21@naver.com>
2022-07-28 17:10:37 +0900

    Add SimpleRouteBuilder

---
+++ demo/src/main/java/com/example/demo/route/SimpleRouteBuilder.java
@@ -0,0 +1,13 @@
+package com.example.demo.route;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleRouteBuilder extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		from("file:C://folder1").to("file:C://folder2");
+	}
+}
